6d659f885dc68011ba2449f31658512c6802fcf5
Dmnk28 <dominik.oesterle@posteo.de>
2021-10-30 18:23:44 +0200

    Changed the Id's so the fcc-Userstory could be fullfilled (id and keyletter have to be unequal)

--- drums/src/data/_drumsArray.js
+++ drums/src/data/_drumsArray.js
@@ -2,55 +2,55 @@ const drumsArray = [
     {
       keyLetter: 'Q',
       keyCode: 81,
-      id: 'Q',
+      id: '1',
       soundUrl: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
     },
     {
       keyLetter: 'W',
       keyCode: 87,
-      id: 'W',
+      id: '2',
       soundUrl: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
     },
     {
       keyLetter: 'E',
       keyCode: 69,
-      id: 'E',
+      id: '3',
       soundUrl: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
     },
     {
       keyLetter: 'A',
       keyCode: 65,
-      id: 'A',
+      id: '4',
       soundUrl: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
     },
     {
       keyLetter: 'S',
       keyCode: 83,
-      id: 'S',
+      id: '5',
       soundUrl: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
     },
     {
       keyLetter: 'D',
       keyCode: 68,
-      id: 'D',
+      id: '6',
       soundUrl: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
     },
     {
       keyLetter: 'Z',
       keyCode: 90,
-      id: 'Z',
+      id: '7',
       soundUrl: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
     },
     {
       keyLetter: 'X',
       keyCode: 88,
-      id: 'X',
+      id: '8',
       soundUrl: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
     },
     {
       keyLetter: 'C',
       keyCode: 67,
-      id: 'C',
+      id: '9',
       soundUrl: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
     }
   ];
