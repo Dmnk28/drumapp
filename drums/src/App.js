a2adc7e5e66b1fc90c9544f358c25f8c1f4a05e6
Dmnk28 <dominik.oesterle@posteo.de>
2021-10-31 13:52:51 +0100

    Started to modify the StandardTheme

--- drums/src/App.js
+++ drums/src/App.js
@@ -1,31 +1,49 @@
 import React, {useState} from 'react';
 
-import { Container } from '@mui/material';
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import DrumPad from './components/DrumPad';
 import ProjectBar from './components/ProjectBar/ProjectBar';
 import drumsArray from './data/_drumsArray';
 
-import logo from './logo.svg';
+import { ThemeProvider } from '@mui/material';
 import './styles/main.css';
+import theme from './components/Theme';
 
 const App = () => {
   const [display, setDisplay] = useState('');
 
   return (
     <div className="App">
-      <Grid container>
-        <Grid item xs={1}>
-          <ProjectBar></ProjectBar>
+      <ThemeProvider theme={theme}>
+      <ProjectBar 
+          title       =   'Build a Drum Machine'
+          institute    =   'FreeCodeCamp'
+          instURL     =   'https://www.freecodecamp.org/'
+          course      =   'Front End Development Libraries Certification'
+          courseURL   =   'https://www.freecodecamp.org/learn/front-end-development-libraries/'
+          userStory   =   'https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-drum-machine'
+          repository  =   'https://github.com/Dmnk28/drumapp'
+          readme      =   'https://github.com/Dmnk28/drumapp/blob/main/README.md'
+      />
+      <Grid container spacing={{xs:3}} id="drum-machine">
+        <Grid item>
+          <Typography variant="h2">Drum Machine</Typography>
+          <Typography variant="h5">A FreeCodeCamp Project</Typography>
+          <div id="display">
+            <h2>{display}</h2>
+          </div>
         </Grid>
-        <Grid item xs={11} id="drum-machine">
-          <div id="display">{display}</div>
-
-          {drumsArray.map(el => (
-            <DrumPad key={el.id} id={el.id} keyLetter={el.keyLetter} keyCode={el.keyCode} soundUrl={el.soundUrl} setDisplay={setDisplay}></DrumPad>
-          ))}
+        <Grid item>
+          <Grid container spacing={{xs:1}} colums={{xs:2}}>
+            {drumsArray.map(el => (
+              <Grid item xs={4} md={4}>
+                <DrumPad key={el.id} id={el.id} keyLetter={el.keyLetter} keyCode={el.keyCode} soundUrl={el.soundUrl} setDisplay={setDisplay}></DrumPad>
+              </Grid>
+            ))}
+          </Grid>
         </Grid>
       </Grid>
+      </ThemeProvider>
     </div>
   );
 }
